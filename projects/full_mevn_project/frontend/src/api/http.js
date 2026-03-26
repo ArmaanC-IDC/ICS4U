9ae37309815246aeaf1bad898611cbc7dd91c3db
Arees Somani <asomani@bayviewglen.ca>
2026-03-26 10:34:43 -0400

    front+back connection

--- projects/full_mevn_project/frontend/src/api/http.js
+++ projects/full_mevn_project/frontend/src/api/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://ics4u-a-25-26-full-mevn-project-backend.onrender.com',
   timeout: 5000
 })
 
